045a22a2a30dc39fc8405f9a3b7c8bcf99959c4f
ooDominikoo <104886373+ooDominikoo@users.noreply.github.com>
2022-06-08 08:47:51 +0200

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,7 +1,17 @@
+from operator import truediv
+
+
 def vithu(): #TicTacToe
     return
 
 def dominik(): #RandomNumberGuesser
+
+    import random
+    randomNumber = r.Next(0, 100)
+    win = False
+    counter = 0
+    
+
     return
 
 #Main game:
